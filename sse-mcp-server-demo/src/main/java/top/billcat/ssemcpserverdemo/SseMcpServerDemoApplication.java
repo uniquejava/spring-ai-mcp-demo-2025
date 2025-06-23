0f5c1a40795cdcc78bbb1c0194cc91f61d64a0c2
cyper <uniquejava@gmail.com>
2025-06-23 11:39:16 +0800

    method tool

--- sse-mcp-server-demo/src/main/java/top/billcat/ssemcpserverdemo/SseMcpServerDemoApplication.java
+++ sse-mcp-server-demo/src/main/java/top/billcat/ssemcpserverdemo/SseMcpServerDemoApplication.java
@@ -1,7 +1,10 @@
 package top.billcat.ssemcpserverdemo;
 
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SseMcpServerDemoApplication {
@@ -10,4 +13,12 @@ public class SseMcpServerDemoApplication {
 		SpringApplication.run(SseMcpServerDemoApplication.class, args);
 	}
 
+	@Bean
+	public ToolCallbackProvider mathTools() {
+		return  MethodToolCallbackProvider.builder().toolObjects(new MathTools()).build();
+	}
+	@Bean
+	public ToolCallbackProvider dateTimeTools() {
+		return  MethodToolCallbackProvider.builder().toolObjects(new DateTimeTools()).build();
+	}
 }
